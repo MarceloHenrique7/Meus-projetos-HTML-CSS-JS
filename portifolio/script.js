652726655e92376582b0a13d4f9f82e813ec12a4
Henrique_Programmer <130008663+MarceloHenrique7@users.noreply.github.com>
2023-06-05 15:48:25 -0300

    atualizando port

--- portifolio/script.js
+++ portifolio/script.js
@@ -1,35 +1,3 @@
-function setProgress(percent) {
-    const progress = document.querySelector('.progress');
-    progress.style.width = percent + '%';
-}
-
-function animateProgressBar(progressElement) {
-    const percent = progressElement.dataset.percent;
-    progressElement.style.width = percent + '%';
-}
-
-function handleIntersect(entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const progressElement = entry.target.querySelector('.progress');
-        animateProgressBar(progressElement);
-        observer.unobserve(entry.target);
-      }
-    });
-  }
-
-const progressBars = document.querySelectorAll('.progress-bar');
-const observerOptions = {
-root: null,
-threshold: 0.5
-};
-
-const observer = new IntersectionObserver(handleIntersect, observerOptions);
-progressBars.forEach((progressBar) => {
-  observer.observe(progressBar);
-});
-
-
 function cliquemenu() {
   if (menu.style.display == 'block') {
     menu.style.display = 'none'
